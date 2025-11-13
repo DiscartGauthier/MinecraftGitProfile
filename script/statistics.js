95fdea450892f45a63ccb6045652df961467ad79
DiscartGauthier <124797296+DiscartGauthier@users.noreply.github.com>
2025-11-13 17:22:07 +0100

    Add English Comment

--- script/statistics.js
+++ script/statistics.js
@@ -1,11 +1,11 @@
-import window from './config.js';
+import window from '../config.js';
 const gitUrl = "https://api.github.com/users/" + window.GitHubUsername + "/repos"
 const gitUrlRepos = "https://api.github.com/repos/" + window.GitHubUsername +"/"
 const minseskinUrl = "https://mineskin.eu/helm/"+ window.MinecraftUsername +"/100.png"
 
 
-
-//Permet de clicker sur une image et va permettre d'avoir input
+// Help for the click on an image and input one
+// Aide pour pouvoir cliquer sur une image et en mettre une
 const fileInput = document.createElement('input');
 fileInput.type = 'file';
 fileInput.accept = 'image/*';
@@ -15,12 +15,14 @@ document.body.appendChild(fileInput);
 
 let targetImg = null;
 
-//En cas de nouveau input d'image il va appelé cette fonction
+// In case a new image input it'll call this function
+// En cas de nouveau input d'image il va appelé cette fonction
 fileInput.addEventListener('change', (e) => {
     const file = e.target.files[0];
     const img = targetImg;
 
-    //On reset le tout au cas où
+    // We will reset every things just in case
+    // On reset le tout au cas où
     if (!file || !img) {
         e.target.value = '';
         targetImg = null;
@@ -30,10 +32,12 @@ fileInput.addEventListener('change', (e) => {
     const reader = new FileReader();
 
     reader.onload = (ev) => {
-        // avec ev on prends l'image upload
+        // With ev we will take the image uploaded
+        // Avec ev on prends l'image upload
         img.src = ev.target.result;
         img.classList.remove('NoData');
-        // on reset après l'avoir changer pour pouvoir le rechanger après
+        // We'll reset after we changed it
+        // On reset après l'avoir changer pour pouvoir le rechanger après
         e.target.value = '';
     };
     reader.readAsDataURL(file);
@@ -44,9 +48,6 @@ fileInput.addEventListener('change', (e) => {
 
 
 
-// Il faudrait faire le moins d'appel à l'API possible donc je dirait qu'on peut en faire 3
-// 1 pour reprendre directement le nom (name)de chaque repos ainsi que le nombre de Star (stargazers_count), la taille (size), le language (language), le nombre de fork (forks_count)
-// Pour ce qui est du nombre de contributeur ou du nombre de commit il faut faire une requête pour chaque repos mais en changeant le endpoint pour ce qui est de contributeur mettre (https://api.github.com/repos/DiscartGauthier/a-delete/contributors), et pour les commits (https://api.github.com/repos/DiscartGauthier/a-delete/commits)
 //En gros t'auras [[name, stars, contributors, commits, language, size, forks], [name2, stars, contributors, commits, language, size, forks], [name3, stars, contributors, commits, language, size, forks]]
 //À mon avis je vais pas utiliser tout les retrieves je vais mettre en instant le nom, star, taille, langue, forks et puis for each et mettre directement la bonne chose et là les retrieve
 
@@ -59,11 +60,12 @@ const repos = await getJson(gitUrl);
 const arr = [];
 let k=0;
 for(const repo of repos) {
-    //ira chercher les JSON pour contributors et Commits
+    // 'll take the json for contributor et commit
+    // Ira chercher les JSON pour contributors et Commits
     const commitsJson = await getJson(gitUrlRepos + repo.name + "/commits");
     const contributorsJson = await getJson(gitUrlRepos + repo.name + "/contributors");
-
-    //reprends tout ce qu'il faut mettre dans la table
+    // Will take every that it needs
+    // Reprends tout ce qu'il faut mettre dans la table
     const repoName = repo.name;
     const repostarsNumber = numberToString(repo.stargazers_count);
     const repoContributorsNumber = numberToString(contributorsJson.length);
@@ -77,7 +79,9 @@ for(const repo of repos) {
     if (k >= 10)
         break;
 }
-/* // For the Test to not make request every time
+/*
+ // For the Test to not make request every time
+ // Pour faire les tests pour ne pas faire de requête à chaque fois
 const arr = [];
 arr[0] = ["a-delete", "0", "1", "5", "C++", "14 K", "1"]
 arr[1] = ["DiscartGauthier", "0", "1", "10", "No code", "20", "0"]
@@ -87,13 +91,15 @@ arr[4] = ["Cocouc", "0", "1", "16", "C#", "47", "0"]
 */
 
 let table = document.querySelector('#tbody');
-//i va permettre de mettre ne muted (estompé) une ligne sur deux
+// i will make each two line in muted
+// i va permettre de mettre ne muted (estompé) une ligne sur deux
 let i = 1;
 for (let subArr of arr) {
     let tr = document.createElement('tr');
     tr.classList.add('stat-row');
     
-    //Va initialiser l'image au début de chaque repos
+    // Will initiate the image in each row
+    // Va initialiser l'image au début de chaque repos
     let th = document.createElement('th');
     th.classList.add('row-icon');
     let dv = document.createElement('div');
@@ -101,9 +107,11 @@ for (let subArr of arr) {
     let img = document.createElement('img');
 
     const imageRepoE = await fetch("./sprites/" + subArr[0] + ".png");
-    //Ici il va check si on a ou pas l'image qui va
+    // Here it'll check if the image exist or not
+    // Ici il va check si on a ou pas l'image qui va
     if (!imageRepoE.ok) {
-        //Si non, on mets l'image par défaut et lui donne de quoi changer en cas de click
+        // If the image don't work it'll put a default img and add a click to modify
+        // Si non, on mets l'image par défaut et lui donne de quoi changer en cas de click
         img.src = 'sprites/Default.png';
         img.classList.add('NoData');
         img.title = 'Clique pour mettre une image';
@@ -130,7 +138,8 @@ for (let subArr of arr) {
     tr.appendChild(th);
 
 
-    //Va initialiser les données dans chaque ligne de repos
+    // Will init the data for each row
+    // Va initialiser les données dans chaque ligne de repos
     for (let j=1; j < subArr.length; j++) {
         let td = document.createElement('td');
         td.classList.add('num');
@@ -148,15 +157,18 @@ for (let subArr of arr) {
 
 //-------------------HELPERS--------------//
 
-//will have the url in parameter
+// Will have the url in parameter and return the json
+// Va prendre en paramètre une url et retourner le json trouver
 async function getJson(url) {
-    //will call the api and return the json
+    // Will call the api and return the json
+    // Va appeler l'api et retourner un json
     const reponse = await fetch(url);
     const repos = await reponse.json();
     return repos;
 }
 
-//will check if the number is more than 999 and put K or M or G or T
+// Will check if the number is more than 999 and put K or M or G or T and will show 3 numbers every time
+// Va vérifier si le nombre est plus grand que 999 et va y mettre un K, M, G ou T et va montrer 3 chiffres à chauqe fois
 function numberToString(number) {
     let string;
     if(number < 1000)
@@ -186,7 +198,8 @@ function numberToString(number) {
     return string;
 }
 
-//Va prendre en parametre un titre et il ne doit pas être plus long que 16 caractère aussi non il va mettre ...
+// Will take in parameter a title and a size and It can't be longer than size-3 carac otherwise it will put ...
+// Va prendre en parametre un titre et une taille et il ne doit pas être plus long que taille-3 caractère aussi non il va mettre ...
 function noMoreCaracter(string, maxsize) {
     if(string == null)
         return "No code"
@@ -210,7 +223,8 @@ if (downloadBtn) {
             useCORS: true
         });
 
-        // Conversion en PNG + déclenchement du téléchargement
+        // Converting in PNG + download
+        // Conversion en PNG + téléchargement
         const dataURL = canvas.toDataURL('image/png');
 
         const link = document.createElement('a');
